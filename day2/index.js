73aac816ef9b1337e3f0199f484b5fe3bb7f44cc
vidyarthi <vidyarthi004@gmail.com>
2023-12-23 12:52:10 +0530

    first push

---
+++ day2/index.js
@@ -0,0 +1,16 @@
+var num1 = 2;
+var num2 = 5;
+console.log(num1 + num2);
+
+
+// Comments - Single line comment , Multi line comments
+/*
+<h1>HTML & CSS Revision</h1>
+<h2>HTML & CSS Revision</h2>
+<h3>HTML & CSS Revision</h3>
+<h4>HTML & CSS Revision</h4>
+<h5>HTML & CSS Revision</h5>
+*/
+
+// Variables and Data Types
+
---
+++ day3/index.js
@@ -0,0 +1,170 @@
+//variable in js
+
+//use of var and let : global scope --var and let -- local scope
+// {
+//     var city = "Saharsa";
+
+// }
+// console.log(city)
+
+// {
+//     let color = "Red";
+//     console.log(color)
+// }
+
+// const aadhar_number = 7215486954789;
+// console.log(aadhar_number)
+
+// const roll;
+// roll = 101;
+
+// const ka local scope hota hai
+
+// {
+//     const id = 10001;
+//     console.log(id)
+// }
+
+//Arithmetic Operators
+// 1. addition
+// var num1 = 10;
+// var num2 = 20;
+// var num = num1 + num2;
+// console.log(num)
+
+//2.
+// var num1 = 20;
+// var num2 = 10;
+// var num = num1 - num2;
+// console.log(num)
+
+//3
+// var num1 = 10;
+// var num2 = 20;
+// var num = num1 * num2;
+// console.log(num)
+
+//4
+// var num1 = 20;
+// var num2 = 10;
+// var num = num1 / num2;
+// console.log(num)
+
+//5
+// var num1 = 55;
+// var num2 = 20;
+// var num = num1 % num2;
+// console.log(num)
+
+//6
+
+// let number = 5;
+// number++;
+// console.log(number)
+
+// let mark = 90;
+// mark--;
+// console.log(mark)
+
+// assignment operator
+// let num = 10;
+// num += 20; // num= num+20
+// console.log(num)
+
+// let y = 50;
+// y -= 20;
+// console.log(y)
+
+// let z = 30;
+// z *= 5;
+// console.log(z)
+
+//Notes
+// Assignment operator
+// x += y : x = x + y
+// x - =y: x = x - y
+// x *= y: x = x * y
+// x /= y: x = x / y
+// x %= y: x = x % y
+// x **= y: x = x ** y
+
+// Data Types in JavaScript
+// let x = 20 + "Rahul";
+// console.log(x)
+
+// let y = "Rahul" + 20;
+// console.log(y)
+
+// let z = 20 + 10 + "Rahul";
+// console.log(z)
+
+// let w = "Rahul" + 20 + 10;
+// console.log(w)
+
+
+// there are 8 types of data type in javascript
+// 1. String
+// 2. Number
+// 3. Biginit
+// 4. Boolean
+// 5. Undefined
+// 6. Null
+// 7. Object
+// 8. Symbol
+
+// let name1 = "Rahul";
+// let name2 = "Raj";
+// console.log(name1 + " " + name2)
+
+// let reply1 = "Hii, Good Morning";
+// let reply2 = 'Hii, "Good Morning"';
+// let reply3 = "Hii, 'Good Morning'";
+// console.log(reply1, reply2, reply3)
+
+// let num1 = 20, num2 = 50, num3 = num1 + num2;
+// console.log(num3)
+
+// let x = 40.00;
+// let y = 40;
+// let z = 40.15;
+// console.log(x, y, z)
+
+// let x = 5, y = 6, z = 10;
+// console.log(x == y)
+// console.log(y == z)
+// console.log(x == z)
+
+// let value1;
+// let value2 = undefined;
+// console.log(value1, value2)
+
+// let value1 = null;
+// console.log(value1)
+
+// Array in JavaScript
+
+// let rollno = [101, 102, 103, 104, 106, 108, 112, 118];
+// console.log(rollno)
+// console.log(rollno[-2])
+// console.log(rollno[0])
+
+//Object in JavaScript
+const car = {
+    brand: "Kia",
+    model: "Sonet",
+    price: 1000000
+};
+console.log(car)
+
+
+
+
+
+
+
+
+
+
+
+
+
