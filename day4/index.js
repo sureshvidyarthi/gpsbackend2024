88a4e062f7450aa1ceb0ac8b3022ef69ce6774cc
vidyarthi <vidyarthi004@gmail.com>
2023-12-24 13:06:59 +0530

    push

---
+++ day4/index.js
@@ -0,0 +1,87 @@
+// Logical Operator
+// let n1 = 10, n2 = 20;
+// console.log(n1 == n2)
+
+// let n1 = 10, n2 = 10, n3 = 15, n4 = 15;
+
+// console.log(n1 == n2 && n3 == n4)
+// console.log(n1 == n3 && n2 == n4)
+// console.log(n1 == n3 || n2 == n4)
+// console.log(n1 == n2 || n3 == n4)
+
+// let n1 = 5, n2 = 10;
+
+// console.log(n1 < 6 && n2 > 9)
+// console.log(n1 > 6 || n2 > 9)
+// console.log(!(n1 < 10))
+
+// Conditional statement
+
+// if else
+
+// let age = 20;
+// if (age >= 18) {
+//     console.log("Drive kr Sakte hai");
+// }
+// else {
+//     console.log("Drive nahi kr Sakte hai");
+// }
+
+// let age = 25;
+// if (age >= 0 && age <= 5) {
+//     console.log("Baby");
+// }
+// else if (age > 5 && age <= 15) {
+//     console.log("Child");
+// }
+// else if (age > 15 && age <= 18) {
+//     console.log("Teen age");
+// }
+// else if (age > 18 && age <= 50) {
+//     console.log("Young")
+// }
+// else if (age > 50) {
+//     console.log("Old")
+// }
+
+
+// let age = prompt("Enter any age");
+// if (age >= 0 && age <= 5) {
+//     console.log("Baby");
+// }
+// else if (age > 5 && age <= 15) {
+//     console.log("Child");
+// }
+// else if (age > 15 && age <= 18) {
+//     console.log("Teen age");
+// }
+// else if (age > 18 && age <= 50) {
+//     console.log("Young")
+// }
+// else if (age > 50) {
+//     console.log("Old")
+// }
+
+
+//Function in JavaScript
+// function myFunction(a, b) {
+//     avg = (a + b) / 2;
+//     return avg;
+// }
+// result = myFunction(40, 20);
+// console.log(result);
+
+//WAP to convert C to F (Temperature)
+
+// function c(a) {
+//     temp = (9 * a + 160) / 5;
+//     return temp;
+// }
+// let b = prompt("Enter temp")
+// result = c(b);
+// console.log("Temperature is: ", result)
+
+//String Method
+
+
+
