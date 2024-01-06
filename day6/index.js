4097e26f5a1268b6e52fbbbc10b4f647e17c4760
vidyarthi <vidyarthi004@gmail.com>
2024-01-06 12:47:05 +0530

    first commit

---
+++ day6/index.js
@@ -0,0 +1,90 @@
+//Formation of String:
+// let start = 5;
+// let string1 = start.toString();
+
+// let finalString = string1.padStart(10, "0");
+// let finalString1 = string1.padEnd(10, "0");
+
+// console.log(finalString);
+// console.log(finalString1);
+
+
+// Array
+// let name1 = "rishi";
+// let name2 = "rishivfjv";
+// let name3 = "rishdmnvdi";
+// let name4 = "rishcgydti";
+// console.log(name1, name2, name3, name4)
+
+
+
+
+// let Car = ["Verna", "Saltos", "Sonet", "Tigor"];
+// Car[2] = "BMW"
+// console.log(Car)
+
+// console.log(Car[3]);
+
+//Convert Array to String
+// let myname = ["Ravi", "Prince", "Priya", "Meera", "Chandani"];
+// console.log(myname.length)
+// let finalString = myname.toString();
+// console.log(finalString.length)
+// console.log(finalString)
+
+
+//JavaScript Random, Math
+// let randomNumber1 = Math.random();
+// let randomNumber2 = Math.floor(Math.random() * 10);
+// let randomNumber3 = Math.floor(Math.random() * 100);
+// let randomNumber4 = Math.floor(Math.random() * 1000);
+// console.log(randomNumber1);
+// console.log(randomNumber2);
+// console.log(randomNumber3);
+// console.log(randomNumber4);
+
+
+//if else
+// let age = prompt("Enter age");
+
+// if (age >= 18) {
+//     console.log("You can Drive");
+// }
+// else {
+//     console.log("You can't Drive");
+// }
+
+// let age = prompt("Enter age");
+// if (age < 18) {
+//     console.log("You are Teenagers");
+// }
+// else if (age >= 18 && age < 50) {
+//     console.log("You are Young")
+// }
+// else {
+//     console.log("You are old")
+// }
+
+//Print Table for 2 up to 10
+let s1 = 2;
+let s2 = 4;
+let s3 = 6;
+let s4 = 8;
+let s5 = 10;
+console.log(s1, s2, s3, s4, s5);
+
+//Loop: For loop
+// let roll = [101, 105, 108, 106];
+// for (let i = 0; i < roll.length; i++) {
+//     console.log(roll[i]);
+// }
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i * 2);
+}
+
+
+
+
+
+
